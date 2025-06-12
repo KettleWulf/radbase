2baf7110adb2c288d2c24fc111d6b4c40f8144db
Olle Wistedt <wistedt@live.com>
2025-06-12 21:42:24 +0200

    Created getMoviesByGenre service

--- src/services/tmdb.services.ts
+++ src/services/tmdb.services.ts
@@ -1,7 +1,18 @@
 import api from "../lib/api"
-import type { Genre, GenreResponse } from "./tmdb.types";
+import { type PaginatedResponse, type Genre, type GenreResponse, type Movie } from "./tmdb.types";
 
 export const getGenres = async (): Promise<Genre[]> => {
 	const res = await api.get<GenreResponse>("/genre/movie/list");
 	return res.data.genres
+}
+
+export const getMoviesByGenre = async (genreId: number, page = 1) => {
+	const res = await api.get<PaginatedResponse<Movie>>("/discover/movie", {
+		params: {
+			with_genres: genreId,
+			page,
+			include_adult: false,
+		}
+	});
+	return res.data;
 }
